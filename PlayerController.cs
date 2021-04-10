251686fded6eb41e45ef7df0c5f3ef68780a883f
9lookch1n <72538275+9lookch1n@users.noreply.github.com>
2021-04-10 20:38:06 +0700

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 moveVelocity;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveVelocity.x = Input.GetAxisRaw("Horizontal");
+        moveVelocity.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("Horizontal", moveVelocity.x);
+        anim.SetFloat("Vertical", moveVelocity.y);
+        anim.SetFloat("Speed", moveVelocity.sqrMagnitude);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * speed * Time.fixedDeltaTime);
+    }
+}
